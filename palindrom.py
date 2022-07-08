2a80cc5306642201aa92b95eab680a8134b1ef85
szd51 <bona.malgorzata@outlook.com>
2022-07-08 20:12:26 +0200

    palindrom.py

---
+++ palindrom.py
@@ -0,0 +1,10 @@
+def czyPalindrom(x):
+    x = x.lower().replace(" ", "")
+    n = len(x)
+    for i in range(n-1):
+        if x[i] != x[n-1-i]:
+            return False
+    return True; 
+print("Program sprawdzajacy czy slowo jest palindromem")
+czyPalindrom ("")
+print("Podane slowo " + ("jest " if(czyPalindrom("kajak")) else "nie jest ") + "palindromem")
